e6e991f4b7bdf4b8ef4fd4cae2a566d182b22b4d
Vyacheslav <dobrov.slava@yandex.ru>
2022-04-08 22:28:57 +0300

    Added import info in main and comments in main

--- PythonCalculator/main_terminal.py
+++ PythonCalculator/main_terminal.py
@@ -1,5 +1,6 @@
 import operations as op
 import calculatortype as ty
+import Komplex_Numbers
 
 loop = 1
 while loop == 1:
@@ -17,3 +18,14 @@ while loop == 1:
             continue
         else:
             break
+
+# operands = Insert_Numbers() # Это в ветку main, для запроса от пользователя двух комплексных чисел и какую операцию он хочет
+# Строчки ниже тоже в main, здесь все вызовы функций. В зависимости от того, что пользователь хочет делать с двумя комплексными числами вызываются соответсвующие функции
+# if operands[2] == "+":
+#     record_in_file(Addition(Take_Rational_Part(operands[0]), Take_Symbol(operands[0]), Take_Imaginary_Part(operands[0]), Take_Rational_Part(operands[1]), Take_Symbol(operands[1]), Take_Imaginary_Part(operands[1])))
+# elif operands[2] == "-":
+#     record_in_file(Deduction(Take_Rational_Part(operands[0]), Take_Symbol(operands[0]), Take_Imaginary_Part(operands[0]), Take_Rational_Part(operands[1]), Take_Symbol(operands[1]), Take_Imaginary_Part(operands[1])))
+# elif operands[2] == "*":
+#     record_in_file(Multiply(Take_Rational_Part(operands[0]), Take_Symbol(operands[0]), Take_Imaginary_Part(operands[0]), Take_Rational_Part(operands[1]), Take_Symbol(operands[1]), Take_Imaginary_Part(operands[1])))
+# else:
+#     record_in_file(division(Take_Rational_Part(operands[0]), Take_Symbol(operands[0]), Take_Imaginary_Part(operands[0]), Take_Rational_Part(operands[1]), Take_Symbol(operands[1]), Take_Imaginary_Part(operands[1])))
