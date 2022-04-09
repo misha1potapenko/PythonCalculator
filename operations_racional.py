db42e7c0ff4b552c44142abc750645e1f584edf4
Daniil <daniil.kobzar299@gmail.com>
2022-04-09 06:50:03 +0200

    def mainterminal created in operations_racional

--- operations_racional.py
+++ operations_racional.py
@@ -1,3 +1,6 @@
+import operations_racional as op
+import sys
+
 def x():
     global firstnum
     firstnum = float(input('Choose first float number: '))
@@ -32,3 +35,25 @@ def res():
         return res
     else:
         print('invalid syntax')
+
+def mainterminal():
+    global file
+    x = op.x()
+    while True:
+        y = op.y()
+        oper = op.selectoperation()
+        res = op.res()
+        file = 'results.txt'
+        with open('results.txt', 'a') as data:
+            data.write(f'The result of {x} {oper} {y} = {res}\n')
+        print(f'The result of {x} {oper} {y} = {res}\n(already written to txt file)' )
+        again = input('Do you want calculate another numbers? Yes/No: ').lower()
+        if again == 'yes':
+            useresult = input('Do you want to use the result of the last operation? (Yes/No): ').lower()
+            if useresult == 'yes':
+                x = res
+                continue
+            else:
+                break              
+        else:   
+            sys.exit()
