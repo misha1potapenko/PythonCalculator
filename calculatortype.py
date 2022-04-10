fbf8cc45d16ca2ff6d5c62e9123b7c967368e961
Daniil <daniil.kobzar299@gmail.com>
2022-04-10 15:04:01 +0200

    Racional changed to rational

--- calculatortype.py
+++ calculatortype.py
@@ -1,3 +1,3 @@
 def type():
-    type = input('What numbers are you going to work with? (Complex/Racional): ').lower()
+    type = input('What numbers are you going to work with? (Complex/Rational): ').lower()
     return type
